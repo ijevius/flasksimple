2435eb70d342324b50edd8fb64b623dae74ef015
Ruslan <47752953+ijevius@users.noreply.github.com>
2023-05-18 17:20:46 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,4 +6,8 @@ def hello():
     return "Hello World! 2"
 
 if __name__ == "__main__":
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()    
     app.run(host='0.0.0.0', port=4567)
