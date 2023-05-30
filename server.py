bfe7426c8ed6b700e010a91a71e1ebb7088c8a02
Ruslan <47752953+ijevius@users.noreply.github.com>
2023-05-30 19:30:52 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! 22 ok deploy"
+    return "Hello World! 23 ok deploy local?"
 
 if __name__ == "__main__":    
     func = None
